cbdae6326b4984e062cf296043378c0de0810972
HSUABI <seongwooksong2017@gmail.com>
2021-05-23 14:28:14 +0900

    올림

---
+++ reg2json.py
@@ -0,0 +1,97 @@
+# -*- encoding: utf-8 -*-
+from fortools import *
+
+def regtojson():
+
+    #경로지정하면 해당경로밑의 하위키(폴더)를 싹 모아서 리스트로 반환
+    def find_subkey(reg, pathlist):
+        for path in pathlist:
+            keyword = path.split("\\")[-1]
+            info = reg.find_key(keyword)
+            
+            list = []
+            for i in info:
+                if (path in i['Search Key Path']) and ( ("\\"+path) not in i['Search Key Path']):
+                    list.append(i['Search Key Path'])
+        return list
+
+
+    ########################레지스트리 하이브파일 읽기######################################################33
+
+    #USRCLASS를 읽어야하는데 못읽음 
+    #reg_file1 = RegistryHive.file_open("C:\\regggg\\gullabjamun.USRCLASS.DAT")#\HKEY_CURRENT_USER\SOFTWARE\Classes
+
+    #레지스트리하이브파일 경로 설정해주세요꼮 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    reg_file1 = RegistryHive.file_open("C:\\regggg\\NTUSER.DAT")
+    reg_file2 = RegistryHive.file_open("C:\\regggg\\SYSTEM")#HKLM
+    reg_file3 = RegistryHive.file_open("C:\\regggg\\SOFTWARE") #\HKEY_LOCAL_MACHINE\SOFTWARE
+    reg_file4 = RegistryHive.file_open("C:\\regggg\\SAM")#HKLM
+
+
+    ############################딕셔너리 설정######################################
+    output= {"Program Installed" : "aa","RecentDocs":"dd","OS Version":"dd"}
+
+
+    #################프로그램 설치 된거 확인#################################
+    a = ["Microsoft\\Windows\\CurrentVersion\\Uninstall","WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"]
+    #a = ["Microsoft\\Windows\\CurrentVersion\\Uninstall"]
+    subkeys = find_subkey(reg_file3,a) #\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
+    programlist=[]
+
+    for key in subkeys:
+        info = reg_file3.find_value(key)
+        info = info[0]
+        if info =={}:
+            continue
+        programlist.append(info)
+    output["Program Installed"]   = programlist
+
+
+    ##################문서 파일 열람#################################
+    docs = []
+    info = reg_file1.Favorite.NTUSER.get_recent_docs()
+    for i in info:
+        docs.append(i)
+
+    info = reg_file1.Favorite.NTUSER.get_ms_office()
+    for i in info:
+        docs.append(i)
+
+
+    info = reg_file1.Favorite.NTUSER.get_HWP()
+    for i in info:
+        docs.append(i)
+
+
+    output["RecentDocs"] = docs
+
+
+    ##################운영체제 버전확인###############################
+    #HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion  
+    os = []
+    info = reg_file3.find_value("Microsoft\\Windows NT\\CurrentVersion")
+    os = info
+    output["OS Version"] = os
+
+
+
+
+
+    #####################json 파일 형식 맞추기위해 짜잘한 작업들하기####################
+    output = str(output)
+    output = output.replace("\"",'changechangechange1')
+    output = output.replace("'",'"')
+    output = output.replace('\\x','\\\\x')
+    output = output.replace('b"\\\\x','"\\\\x')
+    output = output.replace("changechangechange1",'\'')
+
+
+    #반환값은 dictionary를 문자열로 바꿔준 output임
+    #왜냐하면 f.write할때 문자열로 넣어야하기떄문
+    return output
+
+
+testoutput = regtojson()
+f = open("./output.json", 'w', -1,'utf8')
+f.write(testoutput)
+f.close()
